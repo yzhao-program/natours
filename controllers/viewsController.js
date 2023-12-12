464f30ce3d368bf58db1e0dd18da8587e3be573c
yzhao <yzhaogit@gmail.com>
2023-12-11 19:29:51 -0500

    block login and signup page when a user logs in

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -36,15 +36,23 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', {
-    title: 'Log into your account',
-  });
+  if (res.locals.user) {
+    res.redirect('/');
+  } else {
+    res.status(200).render('login', {
+      title: 'Log into your account',
+    });
+  }
 };
 
 exports.getSignUpForm = (req, res) => {
-  res.status(200).render('signup', {
-    title: `Create New Account`,
-  });
+  if (res.locals.user) {
+    res.redirect('/');
+  } else {
+    res.status(200).render('signup', {
+      title: `Create New Account`,
+    });
+  }
 };
 
 exports.getAccount = (req, res) => {
